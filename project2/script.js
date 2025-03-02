3920b3e968725b458b2c482c63179dc34d321033
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-03-03 01:04:34 +0530

    Create script.js

---
+++ project2/script.js
@@ -0,0 +1,9 @@
+function showbar(){
+    var show = document.querySelector(".hide")
+    show.style.display='flex'
+}
+
+function hidenav(){
+    var hide = document.querySelector(".hide")
+    hide.style.display="none"
+}
