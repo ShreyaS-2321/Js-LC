a597283fa2e68b7ac3bf1c10501e2886258db9d6
Shreya Shrivastava <147529420+ShreyaS-2321@users.noreply.github.com>
2025-04-25 01:45:18 +0530

    Create script.js

---
+++ DOMTraversal/script.js
@@ -0,0 +1,19 @@
+const gpElement = document.querySelector(".grand-parent")
+const childElement = document.querySelector(".child")
+// console.log(gpElement)
+// console.log(childElement)
+
+/*
+Types of Traversal
+1)downwards 
+2)upwards
+3)sideways
+*/
+
+// console.log(gpElement.children[0])
+// gpElement.children[0].style.color="Blue"
+// console.log(childElement.parentElement.parentElement)
+// console.log(childElement.closest(".grand-parent"))
+
+// console.log(childElement.nextElementSibling)
+console.log(childElement.previousElementSibling)
